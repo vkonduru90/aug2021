35008c32e9ac625af4574afa3277fcd99d249623
Konduru Venkatesh <vkonduru90@gmail.com>
2021-08-02 09:26:04 +0800

    Session or Local Storage Task

---
+++ index.js
@@ -0,0 +1,17 @@
+function addStudent(name, age, cls, email) {
+  // Logic to Save in Session or Local Storage.
+}
+
+function getStudent(email) {
+  // Logic to Get From Session or Local Storage.
+}
+
+function removeStudent(email) {
+  // Logic to Remove From Session or Local Storage.
+}
+
+function updateStudent(email, updateValues) {
+  // Logic to Update
+}
+
+updateStudent('vkonduru90@gmail.com', {name: 'venkatesh', class: '9th'});
