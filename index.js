b792b471aed779f4c9ba73b190b0e26d03bd3f28
sjkadali <skadalikat@gmail.com>
2021-08-03 17:50:55 -0400

    Latest changes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 function addStudent(name, age, cls, email) {
   // Logic to Save in Session or Local Storage.
   let students = sessionStorage.getItem('students');
@@ -15,17 +16,34 @@ function addStudent(name, age, cls, email) {
 
 function getStudent(email) {
   // Logic to Get From Session or Local Storage.
-    let students = sessionStorage.getItem('students');
-    return students;
+    let students = JSON.parse(sessionStorage.getItem('students'));
+    let student = students.find(student => student.email === email)
+    return student;
 }
 
 function removeStudent(email) {
   // Logic to Remove From Session or Local Storage.
+  let students = JSON.parse(sessionStorage.getItem('students'));
+  let studentsFltd = students.filter(student => student.email !== email);
+
+  sessionStorage.setItem('students',JSON.stringify(studentsFltd));
+  return 'Student Removed Successfully....';
 }
 
 function updateStudent(email, updateValues) {
   // Logic to Update
-}
+  let students = JSON.parse(sessionStorage.getItem('students'));
+  let index = students.findIndex(student => student.email === email);
+  Object.keys(updateValues).forEach(key => students[index][key] = updateValues[key]);
+  sessionStorage.setItem('students', JSON.stringify(students));
+  return 'Student Updated Successfully....';
+} 
 
-// updateStudent('vkonduru90@gmail.com', { name: 'venkatesh', class: '9th' });
 addStudent('KK', 20, 10, 'vkonduru90@gmail.com');
+addStudent('SS', 32, 'Java','ss@gmail.com');
+let student = getStudent( 'vkonduru90@gmail.com');
+console.log('Student Details: ', student);
+
+updateStudent('vkonduru90@gmail.com', { name: 'venkatesh', class: '9th' });
+
+removeStudent('ss@gmail.com');
