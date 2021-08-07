dced19c91beb564b6090bc2a3c507c19c13285f6
geethajeyaraman <geethajeyaraman92@gmail.com>
2021-08-06 20:29:44 -0400

    Update test.js
    test

--- test.js
+++ test.js
@@ -1,4 +1,27 @@
+/*
 var a="test";
-console.log(a);
+console.log(a);*/
 var obj=[{name:"geetha",age:"28", gender:"female", loaction:"tampa"}];
 console.log(obj);
+
+var addition = add(1,4);
+console.log(addition);
+function add(n1,n2){
+    var a=n1+n2;
+    return a;
+}
+ 
+var numArr = [5,10,15,20];
+
+node(25);
+node(27);
+
+
+console.log(numArr);
+function node(a1){
+    
+    var a = a1%5;    
+    if(a==0)
+        numArr.push(a1);
+
+}
