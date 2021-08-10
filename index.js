1577ce879c3feae86e77fce58775573393f9c131
Konduru Venkatesh <vkonduru90@gmail.com>
2021-08-10 10:01:34 +0800

    test

--- index.js
+++ index.js
@@ -1,31 +1,6 @@
-function addStudent(name, age, cls, email) {
-  // Logic to Save in Session or Local Storage.
-  let students = sessionStorage.getItem('students');
-  students = students ? students : '[]';
-  students = JSON.parse(students);
-  students.push({
-    name,
-    age,
-    class: cls,
-    email,
-  });
-  sessionStorage.setItem('students', JSON.stringify(students));
-  return 'Student Added Successfully....';
+function sum(a, b) {
+  return a + b;
 }
 
-function getStudent(email) {
-  // Logic to Get From Session or Local Storage.
-    let students = sessionStorage.getItem('students');
-    return students;
-}
 
-function removeStudent(email) {
-  // Logic to Remove From Session or Local Storage.
-}
-
-function updateStudent(email, updateValues) {
-  // Logic to Update
-}
-
-// updateStudent('vkonduru90@gmail.com', { name: 'venkatesh', class: '9th' });
-addStudent('KK', 20, 10, 'vkonduru90@gmail.com');
+var x = 10;
