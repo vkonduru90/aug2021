47787592bfb6dceeff563c497c3f3d2c386a919b
sjkadali <skadalikat@gmail.com>
2021-08-17 11:58:17 -0400

    Added upload file and file content of Excel file returned as JSON

--- expressWebDev/index.js
+++ expressWebDev/index.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const app = express();
-
-app.use(express.json());
+const multer = require('multer');
+const path = require('path');
 
 const fs = require('fs');
+
+const excelToJson = require('convert-excel-to-json');
+
+const fileStorage = multer.diskStorage({
+    destination: 'uploads',
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname+ '_'+ Date.now()+
+        path.extname(file.originalname));
+    },
+});
+
+const upload = multer({ storage: fileStorage });
+
+let uploadFile = '';
+app.post('/single', upload.single('uploadFile'), (req, res) => {
+    console.log(req.file);
+    if(res) {
+        uploadFile= req.file.filename;
+        let jsonFile = excelToJson({
+            sourceFile: 'uploads/'+uploadFile   ,
+            header: {
+                rows: 1
+            } ,
+            columnToKey: {
+                '*': '{{columnHeader}}'
+            }
+        });    
+        jsonFile ? res.status(200).send(jsonFile) : '';
+        res.status(200).send("File uploaded and converted to json format is: ", jsonFile);   
+    }
+});
 
 app.post('/student', (req, res) => {
     const body = req.body;
